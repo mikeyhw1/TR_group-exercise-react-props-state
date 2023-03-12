0d377697b2e6644c754722d51a9b9d0a1b31a493
mikeyhw1 <70017382+mikeyhw1@users.noreply.github.com>
2023-03-12 16:36:25 +0000

    feat: completed req 1-3

--- src/components/chuck_joke.tsx
+++ src/components/chuck_joke.tsx
@@ -1,9 +1,17 @@
+import Joke from "../joke";
 interface ChuckJokeProps {
-
+    jokeItem: Joke;
 }
 
-const ChuckJoke: React.FC<ChuckJokeProps> = () => <p></p>
-
-
+const ChuckJoke: React.FC<ChuckJokeProps> = ({ jokeItem }) => {
+    const { id, joke } = jokeItem;
+    return (
+        <>
+            <p>Joke ID:{id}</p>
+            <p>{joke}</p>
+            <hr />
+        </>
+    );
+};
 
 export default ChuckJoke;
